b8b645224e5bd273b55142144ccc36b7c9ef4aa4
priyankahpheggar <priyankahp.heggar@gmail.com>
2021-06-11 12:48:05 +0530

    asd
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -1,4 +1,21 @@
 <script runat="server">
 Platform.Load("Core","1.1");
-Write("I am from SSJS");
+Write("Hurray");
+
+try {
+    var ssvar = "I am ssjs var";
+</script>
+
+%%[
+    SET @s1 = "I am a AMPscript var"
+]%%
+<script runat="server">
+var s = Variable.GetValue("@s1");
+Write(s);
+}
+
+Write("")
+ catch (error) {
+    Write(Stringify(error));
+}
 </script>
