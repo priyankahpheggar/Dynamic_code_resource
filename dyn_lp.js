c0dd97aeba7b835817350e9cae21c63253928675
priyankahpheggar <priyankahp.heggar@gmail.com>
2021-06-30 11:17:27 +0530

    s9
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -11,7 +11,7 @@
         var E_Email = e_DE[i].Email;        
        
         
-        d_DE.Rows.Add({FirstName:E_FN,LastName:E_LN,Email:E_Email});
+        d_DE.Rows.Update({FirstName:"Test1",LastName:E_LN,Email:E_Email});
         
     }
     //Write(Stringify(layouts));
