2e7fc550b92e62d608a04ca4661c86d44c042462
priyankahpheggar <priyankahp.heggar@gmail.com>
2021-06-30 10:46:20 +0530

    s2
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -1,4 +1,19 @@
 <script runat="server">
-Platform.Load("Core","1.1");
-Write("Hi Priyanka");
+Platform.Load("core","1.1");
+var DE = "Encrypted_data_Test"
+try {
+
+    var ListOfOptionsDE = DataExtension.Init(DE);
+    var Rows = ListOfOptionsDE.Rows.Retrieve();
+
+    if (Rows.length > 0) {
+        for(var i in Rows) {
+            Write(Rows[i]+ "<br>");
+        }
+    }
+
+
+} catch(e){
+ Write(Stringify(e));
+}
 </script>
