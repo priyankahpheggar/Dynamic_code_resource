98f461edae6f577d89cdc8b5b75fcd4a620f46a8
priyankahpheggar <priyankahp.heggar@gmail.com>
2021-06-30 11:07:50 +0530

    s5
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -1,27 +1,17 @@
-<html>
-    <head>
-    </head>
-    <body>
-        <table style= "border: 1px solid black">
-            <tr>
-                <td>FirstName</td>
-                <td>LastName</td>
-                <td>Email</td>
-              
-            </tr>
-            <script runat=server>  
-                Platform.Load("Core","1");
-                var layouts = DataExtension.Init("Encrypted_data_Test").Rows.Retrieve();
-                for (var i = 0; i < layouts.length; i++)
-                {
-                    Write("<tr>");
-                    Write("<td>" + layouts[i].FirstName + "</td>");
-                    Write("<td>" + layouts[i].LastName + "</td>");
-                    Write("<td>" + layouts[i].Email + "</td>");        
-                    Write("</tr>");
-                }
-                //Write(Stringify(layouts));
-            </script>
-        </table>
-    </body>
-</html>
+
+<script runat=server>  
+    Platform.Load("Core","1");
+    var e_DE = DataExtension.Init("Encrypted_data_Test").Rows.Retrieve();
+    for (var i = 0; i < e_DE.length; i++)
+    {
+        
+        var E_FN = e_DE[i].FirstName;
+        var E_LN = e_DE[i].LastName;
+        var E_Email = e_DE[i].Email;        
+       
+        var d_DE = DataExtension.Init('Decryoted_data_Test');
+        d_DE.Rows.Add({FirstName:'E_FN',LastName:'E_LN',Email:'E_Email'});
+
+    }
+    //Write(Stringify(layouts));
+</script>
