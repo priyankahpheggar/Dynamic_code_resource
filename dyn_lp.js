ac099729b1601b9c8bc5079b6b8e79eb5dd3cc19
priyankahpheggar <priyankahp.heggar@gmail.com>
2021-07-07 17:55:27 +0530

    p1
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -1,7 +1,7 @@
 <script runat='server' language='ampscript'>
 SET @g = GUID()
-SET @sub1 = Uppercase(Substring(@g,1,4))
-SET @sub2 = Lowercase(Substring(@g,5,12))
+SET @sub1 = Uppercase(Substring(@g,1,2))
+SET @sub2 = Lowercase(Substring(@g,3,8))
 SET @sub3 = Concat(@sub1,@sub2)
 SET @sub4 = Replace(@sub3,'-','$')
 SET @psw = @sub4
