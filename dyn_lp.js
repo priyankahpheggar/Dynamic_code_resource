78c8b48840176cf9694e9df9f8e8da08fbf09e1a
priyankahpheggar <priyankahp.heggar@gmail.com>
2021-07-07 17:50:04 +0530

    p1
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -1,71 +1,11 @@
-<script runat="server">
-
-Platform.Load("core", "1.1.1");
-
-var records = retrieveAllRecords("Encrypted_data_Test");
-
-Write(Stringify(records));
-
-function retrieveFieldNames(name) {
-
-    var attr = DataExtension.Retrieve({ Property: "Name", SimpleOperator: "equals", Value: name });
-
-    var de = DataExtension.Init(attr[0].CustomerKey);
-
-    var fields = Base64Decode(de.Fields.Retrieve());
-
-    fields.sort(function (a, b) { return (a.Ordinal > b.Ordinal) ? 1 : -1 });
-
-    var out = [];
-
-    for (k in fields) {
-        out = out.concat(fields[k].Name);
-    }
-
-    return out;
-
-}
-
-function retrieveAllRecords(name) {
-
-    var prox = new Script.Util.WSProxy();
-
-    var cols = retrieveFieldNames(name);
-
-    var config = {
-        name: name,
-        cols: cols
-    }
-
-    var records = [],
-        moreData = true,
-        reqID = data = null;
-
-    while (moreData) {
-
-        moreData = false;
-
-        if (reqID == null) {
-            data = prox.retrieve("DataExtensionObject[" + config.name + "]", config.cols);
-        } else {
-            data = prox.getNextBatch("DataExtensionObject[" + config.name + "]", reqID);
-        }
-
-        if (data != null) {
-            moreData = data.HasMoreRows;
-            reqID = data.RequestID;
-            for (var i = 0; i < data.Results.length; i++) {
-                var result_list = data.Results[i].Properties;
-                var obj = {};
-                for (k in result_list) {
-                    var key = result_list[k].Name;
-                    var val = result_list[k].Value
-                    if (key.indexOf("_") != 0) obj[key] = val;
-                }
-                records.push(obj);
-            }
-        }
-    }
-    return records;
-}
+<script runat='server' language='ampscript'>
+SET @g = GUID()
+SET @sub1 = Uppercase(Substring(@g,1,4))
+SET @sub2 = Lowercase(Substring(@g,5,12))
+SET @sub3 = Concat(@sub1,@sub2)
+SET @sub4 = Replace(@sub3,'-','$')
+SET @psw = @sub4
+SET @epsw = Base64Encode(@psw)
+ Output(Concat('Generated = ', @psw,"<Br>"))
+ Output(Concat('Enc Generated = ', @epsw))
 </script>
