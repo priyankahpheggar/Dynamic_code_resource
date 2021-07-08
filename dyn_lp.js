0b40f1899b4c9524d0a229de1eba6eeed908fe5e
priyankahpheggar <priyankahp.heggar@gmail.com>
2021-07-08 16:19:44 +0530

    p1
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -1,11 +1,83 @@
-<script runat='server' language='ampscript'>
-SET @g = GUID()
-SET @sub1 = Uppercase(Substring(@g,1,4))
-SET @sub2 = Lowercase(Substring(@g,5,8))
-SET @sub3 = Concat(@sub1,@sub2)
-SET @sub4 = Replace(@sub3,'-','$')
-SET @psw = @sub4
-SET @epsw = Base64Encode(@psw)
- Output(Concat('Generated = ', @psw,"<Br>"))
- Output(Concat('Enc Generated = ', @epsw))
+<script runat="server" type="text/javascript">
+Platform.Load("Core","1.1.1");
+        var str1 = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+        var str2 = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+        var str3 = ["1","2","3","4","5","6","7","8","9","0"];
+        var str4 = ["!","@","#","$","%","^","&","*","(",")","_","+","~","`","|","}","{","[","]","?",">","<","/","-","="];
+        var psw = '';    
+            for (var i = 1; i <= 3; i++) 
+            {
+                var w = str1[Math.floor(Math.random() * str1.length)];
+                psw +=  w;
+                var x = str2[Math.floor(Math.random() * str2.length)];
+                psw +=  x;
+                var y = str3[Math.floor(Math.random() * str3.length)];
+                psw +=  y;
+                var z = str4[Math.floor(Math.random() * str4.length)];
+                psw +=  z;
+                Variable.SetValue("@Password",psw);
+            }
+    Write(psw);   
 </script>
+%%[
+IF RequestParameter("submitted") == '1' then    
+SET @TriggeredSendExternalKey = "60349"
+SET @DCF_code = "priyanka.padmanabha@indegene.com"
+SET @register = RequestParameter('register')
+SET @newDCF_reg = RequestParameter('newDCF_reg')
+SET @dont_want_to_reg = RequestParameter('dont_want_to_reg')
+SET @DCF_compare = LookupRows("Master_HCP_DE","DCF_code",@DCF_code)
+SET @rowCount = rowcount(@DCF_compare)
+
+if @rowCount > 0 then
+
+  for @i = 1 to @rowCount do
+    SET @row = row(@DCF_compare, @i) /* get row based on counter */
+    SET @FirstName = field(@DCF_compare,"FirstName")
+    SET @LastName = field(@DCF_compare,"LastName")
+    SET @DCF_code = field(@DCF_compare,"DCF_code")
+        IF @newDCF_reg != '' then
+        InsertDE("User_DE","DCF_code", @newDCF_reg,"FirstName", @FirstName,"LastName", @LastName,"Password", @Password)
+        ELSE
+        InsertDE("User_DE","DCF_code", @DCF_code,"FirstName", @FirstName,"LastName", @LastName,"Password", @Password)
+        ENDIF
+        IF @dont_want_to_reg == "" THEN
+]%%
+<script runat="server">
+                Platform.Load("core","1.1");
+                var data = {
+                    attributes : {
+                      FirstName: Platform.Variable.GetValue("@FirstName"),
+                      LastName: Platform.Variable.GetValue("@LastName"),
+                      Password: Platform.Variable.GetValue("@Password")
+                  },
+                    subscriber : {
+                    EmailAddress: Platform.Variable.GetValue("@DCF_code"),
+                    SubscriberKey: Platform.Variable.GetValue("@DCF_code")
+                  }
+                }
+                var TSD = TriggeredSend.Init(Platform.Variable.GetValue("@TriggeredSendExternalKey"));
+                var Status = TSD.Send(data.subscriber,data.attributes);
+            </script>
+%%[
+    ELSE
+    SET @msg = "dont want to register"
+    ENDIF 
+    next @i
+    ENDIF
+    ENDIF
+]%%
+
+<form action="%%=RequestParameter('PAGEURL')=%%" method="post">
+  <p>Good to register with this information?</p>
+  <input type="radio" name="register" value="%%=v(@DCF_code)=%%">
+  <label>Register with this Email address</label> <input type="text" name="DCF_code" value="%%=v(@DCF_code)=%%"/><br>
+  <input type="radio" name="register" value="%%=v(@newDCF_reg)=%%">
+  <label>Register with another email address</label> <input type="text" name="newDCF_reg" /><br>
+  <input type="radio" name="register" value="%%=v(@dont_want_to_reg)=%%">
+  <label>Not want to register</label><br>
+  <input type="hidden" name="submitted" value="1"><br>
+  <input type="submit" value="Submit">
+</form>
+
+
