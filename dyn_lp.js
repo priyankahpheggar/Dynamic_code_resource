419e1ba4f3971200d28cf41e8cdd07153e2b6ae8
priyankahpheggar <priyankahp.heggar@gmail.com>
2021-07-09 15:01:49 +0530

    sdsd
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -37,13 +37,7 @@ SET @register = RequestParameter('register')
 SET @newDCF_reg = RequestParameter('newDCF_reg')
 
         IF @register == "current email address" OR @register == "new email address" THEN
-
-
-        if Empty(@newDCF_reg) or @newDCF_reg == '' or @newDCF_reg == null then
-            InsertDE("User_DE","DCF_code", @DCF_code,"EmailAddress", @EmailAddress,"FirstName", @FirstName,"LastName", @LastName,"Password", @Password)  
-        else
-        InsertDE("User_DE","DCF_code", @DCF_code,"EmailAddress", @newDCF_reg,"FirstName", @FirstName,"LastName", @LastName,"Password", @Password)  
-        endif
+        InsertDE("User_DE","DCF_code", @DCF_code,"EmailAddress", @EmailAddress,"EmailAddress", @newDCF_reg,"FirstName", @FirstName,"LastName", @LastName,"Password", @Password)  
  
         
 ]%%
