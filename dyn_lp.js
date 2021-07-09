afb3c7761fdf767c498c36b70a7ac1198046ccd5
priyankahpheggar <priyankahp.heggar@gmail.com>
2021-07-09 14:07:14 +0530

    sdsd
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -26,12 +26,11 @@ Platform.Load("Core","1.1.1");
 
 SET @DCF_code = "D_001"
 SET @DCF_compare = LookupRows("Master_HCP_DE","DCF_code",@DCF_code)
+SET @row = row(@DCF_compare,1)
+SET @FirstName = field(@DCF_compare,"FirstName")
+SET @LastName = field(@DCF_compare,"LastName")
+SET @EmailAddress = field(@DCF_compare,"EmailAddress")
 
-    SET @row = row(@DCF_compare,1)
-    SET @FirstName = field(@DCF_compare,"FirstName")
-    SET @LastName = field(@DCF_compare,"LastName")
-    SET @DCF_code = field(@DCF_compare,"DCF_code")
-    SET @EmailAddress = field(@DCF_compare,"EmailAddress")
 IF RequestParameter('submitted') == '1' then    
 SET @TriggeredSendExternalKey = "60349"
 SET @register = RequestParameter('register')
