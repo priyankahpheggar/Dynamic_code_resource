28d0e461d38572e4481c3c448c2242a4a3b6debd
priyankahpheggar <priyankahp.heggar@gmail.com>
2021-07-09 13:55:56 +0530

    p1
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -26,13 +26,6 @@ Platform.Load("Core","1.1.1");
 %%[
 
 SET @DCF_code = "D_001"
-SET @DCF_compare = LookupRows("Master_HCP_DE","DCF_code",@DCF_code)
-SET @row = row(@DCF_compare,1) 
-SET @FirstName = field(@DCF_compare,"FirstName")
-SET @LastName = field(@DCF_compare,"LastName")
-SET @DCF_code = field(@DCF_compare,"DCF_code")
-SET @EmailAddress = field(@DCF_compare,"EmailAddress")
- 
 
 IF RequestParameter('submitted') == '1' then    
 SET @TriggeredSendExternalKey = "60349"
@@ -40,6 +33,12 @@ SET @register = RequestParameter('register')
 SET @newDCF_reg = RequestParameter('newDCF_reg')
 SET @dont_want_to_reg = RequestParameter('dont_want_to_reg')
 
+SET @DCF_compare = LookupRows("Master_HCP_DE","DCF_code",@DCF_code)
+SET @row = row(@DCF_compare,1) 
+SET @FirstName = field(@DCF_compare,"FirstName")
+SET @LastName = field(@DCF_compare,"LastName")
+SET @DCF_code = field(@DCF_compare,"DCF_code")
+SET @EmailAddress = field(@DCF_compare,"EmailAddress")
         IF @register == "current email address" OR @register == "new email address" THEN
         InsertDE("User_DE","DCF_code", @DCF_code,"EmailAddress", (@EmailAddress,@newDCF_reg),"FirstName", @FirstName,"LastName", @LastName,"Password", @Password)   
         
