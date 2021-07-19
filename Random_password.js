2a1ade97d140182e4c9cb416458abd5edb25d0cd
priyankahpheggar <priyankahp.heggar@gmail.com>
2021-07-19 16:42:06 +0530

    g
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- Random_password.js
+++ Random_password.js
@@ -1,59 +1,73 @@
-<script runat="server" type="text/javascript">
-Platform.Load("Core","1.1.1");
-        var str1 = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-        var str2 = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-        var str3 = ["1","2","3","4","5","6","7","8","9","0"];
-        var str4 = ["!","@","#","$","%","^","&","*","(",")","_","+","~","`","|","}","{","[","]","?",">","<","/","-","="];
-        var psw = '';    
-            for (var i = 1; i <= 3; i++) 
-            {
-                var w = str1[Math.floor(Math.random() * str1.length)];
-                psw +=  w;
-                var x = str2[Math.floor(Math.random() * str2.length)];
-                psw +=  x;
-                var y = str3[Math.floor(Math.random() * str3.length)];
-                psw +=  y;
-                var z = str4[Math.floor(Math.random() * str4.length)];
-                psw +=  z;
-            }
-        var decrypted_psw = psw;
-        var Encrypted_psw = Base64Encode(decrypted_psw);
-        Variable.SetValue("@Password",Encrypted_psw)
-    Write(Encrypted_psw); 
-  
-</script>
 %%[
+SET @dcf = RequestParameter('dcf')
+SET @mail_seq = RequestParameter('mail_seq')
+SET @site = RequestParameter('site')
+SET @decrypt_dcf = Base64Decode(@dcf)
 
-SET @DCF_code = "D_001"
-SET @DCF_compare = LookupRows("Master_HCP_DE","DCF_code",@DCF_code)
-SET @row = row(@DCF_compare,1)
-SET @FirstName = field(@row,"FirstName")
-SET @LastName = field(@row,"LastName")
-SET @EmailAddress = field(@row,"EmailAddress")
+SET @dcf_compare = LookupRows("MASTER_Account","AABP_DCF_Doctor_Code",@decrypt_dcf)
+SET @row = row(@dcf_compare,1)
+SET @FirstName = field(@row,"First_name")
+SET @Amgen_id = field(@row,"Amgen_id")
+SET @Account_id = field(@row,"Account_id")
+IF @mail_seq == 1 THEN
+SET @EmailAddress = field(@row,"Mail")    
+elseif @mail_seq == 2 THEN
+SET @EmailAddress = field(@row,"Mail2")  
+else
+SET @EmailAddress = field(@row,"Mail3")  
+endif
 
-IF RequestParameter('submitted') == '1' then    
-SET @TriggeredSendExternalKey = "60349"
+IF RequestParameter('submitted') == '1' THEN    
+SET @TriggeredSendExternalKey = ""
 SET @register = RequestParameter('register')
 SET @newDCF_reg = RequestParameter('newDCF_reg')
 
+      IF @register == "not to register" THEN
+      Redirect('https://www.example.com')
+      ENDIF
+
       IF @register == "current email address" OR @register == "new email address" THEN
 
-      InsertDE("User_DE","DCF_code", @DCF_code,"EmailAddress", @EmailAddress,"FirstName", @FirstName,"LastName", @LastName,"Password", @Password)
+      if NOT Empty(@newDCF_reg) then
+     
+      InsertDE("DE_MASTER_AMOVIG","AABP_DCF_Doctor_Code", @decrypt_dcf,"Login_id", @newDCF_reg,"First_name", @FirstName,"Amgen_id", @Amgen_id,"Account_id", @Account_id)
  
+      else
+         
+      InsertDE("DE_MASTER_AMOVIG","AABP_DCF_Doctor_Code", @decrypt_dcf,"Login_id", @EmailAddress,"First_name", @FirstName,"Amgen_id", @Amgen_id,"Account_id", @Account_id) 
+      endif
         
 ]%%
 <script runat="server">
                 Platform.Load("core","1.1");
-                var data = {
+                var newDCF_reg = variable.GetValue('@newDCF_reg');
+                var EmailAddress = variable.GetValue('@EmailAddress');
+                if(newDCF_reg !== null && newDCF_reg !== '') 
+                {
+                  var data = {
                     attributes : {
-                      FirstName: Platform.Variable.GetValue("@FirstName"),
-                      LastName: Platform.Variable.GetValue("@LastName"),
-                      Password: Platform.Variable.GetValue("@Password")
+                      First_name: Platform.Variable.GetValue("@FirstName")
                   },
                     subscriber : {
-                    EmailAddress: Platform.Variable.GetValue("@EmailAddress"),
-                    SubscriberKey: Platform.Variable.GetValue("@EmailAddress")
+                    EmailAddress: newDCF_reg,
+                    SubscriberKey: newDCF_reg
                   }
+                }
+                  
+                }
+
+               else 
+                {
+                  var data = {
+                    attributes : {
+                      First_name: Platform.Variable.GetValue("@FirstName")
+                  },
+                    subscriber : {
+                    EmailAddress: EmailAddress,
+                    SubscriberKey: EmailAddress
+                  }
+                }
+                  
                 }
                 var TSD = TriggeredSend.Init(Platform.Variable.GetValue("@TriggeredSendExternalKey"));
                 var Status = TSD.Send(data.subscriber,data.attributes);
@@ -66,7 +80,7 @@ ENDIF
 <!DOCTYPE html>
 <html>
 <body>
-<form action="%%=RequestParameter('PAGEURL')=%%" method="post">
+<form action="%%=RequestParameter('PAGEURL')=%%" method="get">
   <p>Good to register with this information?</p>
   <input type="radio" name="register" value="current email address">
   <label>Register with this Email address</label> <input type="text" value="%%=v(@EmailAddress)=%%" style="width:20%;"/><br>
