5e0af24fb7a78b04b84cb0cc4583fc6f1a8e07eb
priyankahpheggar <priyankahp.heggar@gmail.com>
2021-07-23 17:30:30 +0530

    e
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -2,7 +2,7 @@
     SET @emailaddr = 'priyankahp.heggar@gmail.com'
     SET @password = 'priya@1592' 
     SET @salt = '56f4367d891f0111' 
-    SET @initVector = '4963b7334a46352623252955df21d7f3' 
+    SET @initVector = '123456789a0123456789bcdef1234567' 
     SET @encData = EncryptSymmetric(@emailaddr, "aes", @null, @password, @null, @salt, @null, @initVector) 
 
   set @decAES = DecryptSymmetric(@encData, "aes", @null, @password, @null, @salt, @null, @initVector)
