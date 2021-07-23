59f609def61fae4d97f7d026a80df6a1cce76521
priyankahpheggar <priyankahp.heggar@gmail.com>
2021-07-23 17:22:09 +0530

    g
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -1,7 +1,7 @@
 %%[
     SET @emailaddr = 'priyankahp.heggar@gmail.com'
     SET @password = 'priya@1592' 
-    SET @salt = '56e891ccfe482e5f' 
+    SET @salt = '56f4367d891f011g' 
     SET @initVector = '4963b7334a46352623252955df21d7f3' 
     SET @encData = EncryptSymmetric(@emailaddr, "aes", @null, @password, @null, @salt, @null, @initVector) 
 
