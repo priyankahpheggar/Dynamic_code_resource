bb95844fd74a597201b40377927e498458ec3c16
priyankahpheggar <priyankahp.heggar@gmail.com>
2021-07-27 11:36:41 +0530

    p1
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -1,9 +1,11 @@
 %%[
-    /* SET @emailaddr = 'priyankahp.heggar@gmail.com' */
-    SET @encData = EncryptSymmetric("priyankahp.heggar@gmail.com","des;mode=ecb;padding=zeros",@null,"4963b7334a46352623252955df21d7f3")
+    /* SET @emailaddr = 'priyankahp.heggar@gmail.com'
+    SET @encData = EncryptSymmetric("priyankahp.heggar@gmail.com","des;mode=ecb;padding=zeros",@null,"4963b7334a46352623252955df21d7f3") */
    /* SET @decAES = DecryptSymmetric(@encData, "AES", @null, "4963b7334a46352623252955df21d7f3", "Priyanka_salt", @null, @null, "4963b7334a46352623252955df21d7f3") */
+   
 
 ]%%
+%%=EncryptSymmetric("priyankahp.heggar@gmail.com","des;mode=ecb;padding=zeros",@null,"4963b7334a46352623252955df21d7f3")=%%
 encr = %%=v(@encData)=%%<br/>
 decr = %%=v(@decAES)=%%
 
