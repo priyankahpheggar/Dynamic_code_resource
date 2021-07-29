8fe467566bf46e9bcecfb9bce5cf3870706152a0
priyankahpheggar <priyankahp.heggar@gmail.com>
2021-07-29 11:04:15 +0530

    p1
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -1,6 +1,6 @@
 %%[
     SET @emailaddr = 'priyankahp.heggar@gmail.com'
-    SET @encrypt_data = Base64Encode(@emailaddr)
+    SET @encrypt_data = Base64Encode(@emailaddr,"_743677397A244326")
     SET @decrypt_data = Base64Decode(@encrypt_data,'UTF-8', 1)
 ]%%
 encrypted_data = %%=v(@encrypt_data)=%%<br/>
