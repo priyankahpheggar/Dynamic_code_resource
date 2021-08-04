07520d1bf2c1cc4cd5fdf8d41bdcbe79db68dcc0
priyankahpheggar <priyankahp.heggar@gmail.com>
2021-08-04 16:32:52 +0530

    sds
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -50,35 +50,7 @@ SET @new_email_reg = RequestParameter('email_Id')
                 var data = {};
                 Write(new_email_reg);
                 Write(EmailAddress);
-                /* if(new_email_reg != null && new_email_reg != '') 
-                {
-                  var data = {
-                    attributes :  {
-                      First_name: Platform.Variable.GetValue("@FirstName")
-                                  },
-                    subscriber : {
-                    EmailAddress: new_email_reg,
-                    SubscriberKey: new_email_reg
-                                  }
-                              }
-                  
-                }
-
-               else 
-                {
-                  var data =  {
-                    attributes :  {
-                      First_name: Platform.Variable.GetValue("@FirstName")
-                                  },
-                    subscriber :  {
-                    EmailAddress: EmailAddress,
-                    SubscriberKey: EmailAddress
-                                  }
-                              }
-                  
-                }
-                var TSD = TriggeredSend.Init(Platform.Variable.GetValue("@TriggeredSendExternalKey"));
-                var Status = TSD.Send(data.subscriber,data.attributes);
+                
 </script> 
 %%[
     Redirect('https://cloud.amgenmail.com/aimovig_thankyou')
