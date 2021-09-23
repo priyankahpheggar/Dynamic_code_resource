edd54f8fea27f9db22f58029fab73ff550745a2c
priyankahpheggar <priyankahp.heggar@gmail.com>
2021-09-23 13:47:03 +0530

    d
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -1,24 +1,42 @@
+%%[
+SET @DCF_Code = RequestParameter('DCF_Code')
+SET @Membership_type = RequestParameter('Membership_type')
+]%%
 <script runat="server">
+
 Platform.Load("Core","1.1.1");
+
 var json = Platform.Request.GetPostData();
-var products = Platform.Function.ParseJSON(json);
+var jsonObj = Platform.Function.ParseJSON(json);
 
-if (products.length > 0) {
-  for (var i = 0; i < products.length; i++ ) {
+if (jsonObj.length > 0) {
 
-      var item = products[i];
-      Platform.Variable.SetValue("@Prod_ID",item['Prod_ID']);
-      Platform.Variable.SetValue("@Prod_Name",item['Prod_Name']);
-      Platform.Variable.SetValue("@Prod_Desc",item['Prod_Desc']);
-</script>
+  for (var i = 0; i < jsonObj.length; i++ ) {
 
-      <br>Prod_ID = %%=v(@Prod_ID)=%%  || Prod_Name = %%=v(@Prod_Name)=%% || Prod_Desc = %%=v(@Prod_Desc)=%%
+      var item = jsonObj[i];
 
-<script runat="server">
+      Platform.Variable.SetValue("@OrderId",item['OrderId']);
+      Platform.Variable.SetValue("@Quantity",item['Quantity']);
+  </script> 
+  %%[
+/* SET @ProductName = Lookup("Material_Information_DE","ProductName","Order_ID", @OrderId) */
+]%%
+  <div>
+  資材名 %%=v(@OrderId)=%%         部数 %%=v(@Quantity)=%%
+  </div>   
+<script>
   }
-} 
-else 
-{
-  Write("no products found");
+
+} else {
+
+  Write("no products found")
+
 }
 </script>
+
+
+
+
+
+
+
