bafeb87e880bac8239ad05782182fd66275f27a4
priyankahpheggar <priyankahp.heggar@gmail.com>
2021-09-23 13:48:03 +0530

    df
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -2,6 +2,8 @@
 SET @DCF_Code = RequestParameter('DCF_Code')
 SET @Membership_type = RequestParameter('Membership_type')
 ]%%
+%%=v(@DCF_Code)=%% 
+%%=v(@Membership_type)=%% 
 <script runat="server">
 
 Platform.Load("Core","1.1.1");
