101c35098ccf14131703e60ff31f1411553fbc0a
priyankahpheggar <priyankahp.heggar@gmail.com>
2021-09-23 13:40:53 +0530

    e
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -1,1 +1,24 @@
-%%=RequestParameter('ProductID')=%%
+<script runat="server">
+Platform.Load("Core","1.1.1");
+var json = Platform.Request.GetPostData();
+var products = Platform.Function.ParseJSON(json);
+
+if (products.length > 0) {
+  for (var i = 0; i < products.length; i++ ) {
+
+      var item = products[i];
+      Platform.Variable.SetValue("@Prod_ID",item['Prod_ID']);
+      Platform.Variable.SetValue("@Prod_Name",item['Prod_Name']);
+      Platform.Variable.SetValue("@Prod_Desc",item['Prod_Desc']);
+</script>
+
+      <br>Prod_ID = %%=v(@Prod_ID)=%%  || Prod_Name = %%=v(@Prod_Name)=%% || Prod_Desc = %%=v(@Prod_Desc)=%%
+
+<script runat="server">
+  }
+} 
+else 
+{
+  Write("no products found");
+}
+</script>
