ded48c892c562afce1570d6f5a5dcd5740b1a9f2
priyankahpheggar <priyankahp.heggar@gmail.com>
2021-09-23 13:59:02 +0530

    df
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -3,4 +3,20 @@ SET @DCF_Code = RequestParameter('DCF_Code')
 SET @Membership_type = RequestParameter('Membership_type')
 ]%%
 %%=v(@DCF_Code)=%% 
-%%=v(@Membership_type)=%%
+%%=v(@Membership_type)=%% 
+<script runat="server">
+
+Platform.Load("Core","1.1.1");
+
+var json = Platform.Request.GetPostData();
+var jsonObj = Platform.Function.ParseJSON(json);
+
+if (jsonObj.length > 0) {
+Write("products found")
+
+} else {
+
+  Write("no products found")
+
+}
+</script>
