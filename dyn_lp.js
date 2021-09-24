afa5fe40b0f7747a30134c8a5f8450b270083769
priyankahpheggar <priyankahp.heggar@gmail.com>
2021-09-24 12:57:46 +0530

    f
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -22,7 +22,7 @@ if (jsonObj.length > 0) {
       
   </script> 
  %%[
- SET @Entry_DE = UpsertData("User_Material_Order_Entry_data",1,"Order_ID", @Order_ID,"Membership_type", @Membership_type,"Material_Name", @material_name,"Quantity", @Quantity,"Full_name", "Priyanka","EmailAddress","priyanka.padmanabha@indegene.com","DCF_Code", "12345")
+ SET @Entry_DE = UpsertData("User_Material_Order_Entry_data",2,"Order_ID", @Order_ID,"Material_Name", @material_name,"Membership_type", @Membership_type,"Quantity", @Quantity,"Full_name", "Priyanka","EmailAddress","priyanka.padmanabha@indegene.com","DCF_Code", "12345")
  ]%%
   <div>
   資材名 %%=v(@OrderId)=%%         部数 %%=v(@Quantity)=%%
