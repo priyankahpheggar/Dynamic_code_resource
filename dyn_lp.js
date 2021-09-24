23e1d7b0cdf7a29271f5f259ac8ddcc2553a6b99
priyankahpheggar <priyankahp.heggar@gmail.com>
2021-09-24 13:24:19 +0530

    f
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -4,6 +4,8 @@ SET @Membership_type = RequestParameter('Membership_type')
 SET @low = 11111
 SET @high = 99999
 SET @Order_ID = Random(@low,@high)
+set @systemDate  = Now(1)
+set @Order_placed_date =  SystemDateToLocalDate(@systemDate)
 ]%%
 
 
@@ -23,7 +25,7 @@ if (jsonObj.length > 0) {
   </script> 
  
 %%[
- SET @Entry_DE = UpsertData("User_Material_Order_Entry_data",2,"Order_ID", @Order_ID,"Material_Name", @material_name,"Membership_type", @Membership_type,"Quantity", @Quantity,"Full_name", "Priyanka","EmailAddress","priyanka.padmanabha@indegene.com","DCF_Code", "12345")
+ SET @Entry_DE = UpsertData("User_Material_Order_Entry_data",2,"Order_ID", @Order_ID,"Material_Name", @material_name,"Membership_type", @Membership_type,"Quantity", @Quantity,"Full_name", "Priyanka","EmailAddress","priyanka.padmanabha@indegene.com","DCF_Code", "12345","Order_placed_date", @Order_placed_date)
  ]%%
   <div>
   資材名 %%=v(@OrderId)=%%         部数 %%=v(@Quantity)=%%
