632bf090c92a79dacc8104cd898bae5ae9cce1a3
priyankahpheggar <priyankahp.heggar@gmail.com>
2021-09-24 14:14:29 +0530

    f
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -12,23 +12,19 @@ set @Order_placed_date =  SystemDateToLocalDate(@systemDate)
 <script runat="server">
 Platform.Load("Core","1.1.1");
 var json = Platform.Request.GetPostData();
-var jsonObj = Platform.Function.ParseJSON(json);
-Platform.Variable.SetValue("@jsonObj",jsonObj);
-if (jsonObj.length > 0) {
+var Material_info = Platform.Function.ParseJSON(json);
+Platform.Variable.SetValue("@Material_info",Material_info);
+if (Material_info.length > 0) {
 
-  for (var i = 0; i < jsonObj.length; i++ ) {
+  for (var i = 0; i < Material_info.length; i++ ) {
 
-      var item = jsonObj[i];
+      var item = Material_info[i];
       Platform.Variable.SetValue("@material_name",item['material_name']);
       Platform.Variable.SetValue("@Quantity",item['Quantity']);
       
   </script> 
- 
-%%[
- SET @Entry_DE = UpsertData("User_Material_Order_Entry_data",2,"Order_ID", @Order_ID,"Material_Name", @material_name,"Membership_type", @Membership_type,"Quantity", @Quantity,"Full_name", "Priyanka","EmailAddress","priyanka.padmanabha@indegene.com","DCF_Code", "12345","Order_placed_date", @Order_placed_date)
- ]%%
   <div>
-  資材名 %%=v(@OrderId)=%%         部数 %%=v(@Quantity)=%%
+  資材名 %%=v(@material_name)=%%         部数 %%=v(@Quantity)=%%
   </div>   
 <script runat="server">
   }
@@ -39,5 +35,9 @@ if (jsonObj.length > 0) {
 
 }
 </script>
+
+%%[
+ SET @Entry_DE = UpsertData("User_Material_Order_Entry_data",2,"Order_ID", @Order_ID,"Material_Name", @material_name,"Membership_type", @Membership_type,"Quantity", @Quantity,"Full_name", "Priyanka","EmailAddress","priyanka.padmanabha@indegene.com","DCF_Code", "12345","Order_placed_date", @Order_placed_date)
+ ]%%
 
 
