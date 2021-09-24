827066936828874467a7d4900b22a45c3be1fd5c
priyankahpheggar <priyankahp.heggar@gmail.com>
2021-09-24 12:48:56 +0530

    f
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -1,1 +1,37 @@
-%%=RequestParameter('json')=%%
+%%[
+SET @DCF_Code = RequestParameter('DCF_Code')
+SET @Membership_type = RequestParameter('Membership_type')
+SET @low = 11111
+SET @high = 99999
+SET @Order_ID = Random(@low,@high)
+]%%
+
+
+<script runat="server">
+Platform.Load("Core","1.1.1");
+var json = Platform.Request.GetPostData();
+var jsonObj = Platform.Function.ParseJSON(json);
+Platform.Variable.SetValue("@jsonObj",jsonObj);
+if (jsonObj.length > 0) {
+
+  for (var i = 0; i < jsonObj.length; i++ ) {
+
+      var item = jsonObj[i];
+      Platform.Variable.SetValue("@OrderId",item['OrderId']);
+      Platform.Variable.SetValue("@Quantity",item['Quantity']);
+      
+  </script> 
+ SET @Entry_DE = UpsertData("User_Material_Order_Entry_data",1,"Order_ID", @Order_ID,"Membership_type", @Membership_type,"Material_Name", @OrderId,"Quantity", @Quantity,"Full_name", "Priyanka","EmailAddress","priyanka.padmanabha@indegene.com","DCF_Code", "12345")
+  <div>
+  資材名 %%=v(@OrderId)=%%         部数 %%=v(@Quantity)=%%
+  </div>   
+<script runat="server">
+  }
+
+} else {
+
+  Write("no products found")
+
+}
+</script>
+
