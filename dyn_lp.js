3e8b62409be6f9d3375d7da35d9aacf2b18a0466
priyankahpheggar <priyankahp.heggar@gmail.com>
2021-09-28 21:19:13 +0530

    gf
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -2,10 +2,8 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script>
 $(document).ready(function(){
-  $("form").submit(function(){
-    alert("Submitted");
+  $('form#x1').submit();
   });
-});
 </script>
 
 <form action="%%=RedirectTo(CloudPagesURL(2055,'email_name',@EmailAddress,'gender',@Gender,'age',@Age,'residence',@Residence,'event_name',@Q5,'headache_freq',@Q6,'doctor_diagnose',@Q7,'headache_specialist',@Q8,'questions_textarea',@Q9))=%%" method="post" name="x1" id="x1" enctype="multipart/form-data" class="formmailer-form " data-max-total-filesize="5242880" novalidate>
