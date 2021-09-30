bb1ec3e462297fd3ed1faa39d12c6b04e1f45251
priyankahpheggar <priyankahp.heggar@gmail.com>
2021-09-30 14:58:20 +0530

    fdfd
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -1,3 +1,4 @@
 %%[
-SET @x = InsertData("Test_Internal_Proofing_DE","Email","raja.karuppaiya@indegene.com","FirstName","raja","LastName","Karuppaiya")
+set @upsertCount = upsertData("Test_Internal_Proofing_DE",1,"Email","raja.karuppaiya@indegene.com","FirstName","raja","LastName","Karuppaiya")
 ]%%
+UpsertCount: %%=v(@upsertCount)=%%
