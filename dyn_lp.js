7c1e6d516d9da39fbb20696696dd08b64f03e8de
priyankahpheggar <priyankahp.heggar@gmail.com>
2021-09-30 14:59:53 +0530

    fgg
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -1,4 +1,13 @@
+<script runat="server">
+Platform.Load("Core","1.1");
+try{
+</script>
+
 %%[
 set @upsertCount = upsertData("QWR",1,"Email","raja.karuppaiya@indegene.com","FirstName","raja","LastName","Karuppaiya")
 ]%%
 UpsertCount: %%=v(@upsertCount)=%%
+
+<script runat="server">
+} catch(Error) { Write(Stringify(Error)); }
+</script>
