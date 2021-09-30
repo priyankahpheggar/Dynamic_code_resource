33ea5a3b83b7f18e138eb1dadd8f8bd858257d46
priyankahpheggar <priyankahp.heggar@gmail.com>
2021-09-30 14:44:31 +0530

    ghg
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<p>Select a new car from the list.</p>
-
-
-<select id="mySelect" onchange="myFunction()">
-  <option value="Audi">Audi</option>
-  <option value="BMW">BMW</option>
-  <option value="Mercedes">Mercedes</option>
-  <option value="Volvo">Volvo</option>
-</select>
-
 
    <script runat="server">
                 Platform.Load("core","1.1");
@@ -24,8 +11,8 @@ var targetDE = DataExtension.Init("Test_Internal_Proofing_DE");
 targetDE.Rows.Add(Addrecords);
 }
     
-  </script>
+</script>
 
- 
 
-</html>
+
+
