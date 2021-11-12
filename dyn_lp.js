345de45a44b0d1445038db3c63a8d75fd158117a
priyankahpheggar <priyankahp.heggar@gmail.com>
2021-11-12 16:41:51 +0530

    dgf
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -1,6 +1,6 @@
 %%[
 SET @guid = Replace(GUID(),'-','')
 SET @Order_ID = Substring(@guid,1,6)
-SET @Order_ID = Concat("AMV_",@Order_ID)
+SET @Order_ID = Concat("AMV-",@Order_ID)
 ]%%
 %%=v(@Order_ID)=%%
