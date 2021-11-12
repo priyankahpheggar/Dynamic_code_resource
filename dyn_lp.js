d6f2bc9f91031d6f65052174291be095601d168e
priyankahpheggar <priyankahp.heggar@gmail.com>
2021-11-12 19:52:32 +0530

    sf
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -1,7 +1,8 @@
 %%[
-    SET @amount_of_deliveries = RequestParameter('X')
-  SET @sl = Length(@amount_of_deliveries)
-  IF @s1 > 3 THEN
+    SET @amount_of_deliveries = '123'
+   SET @sl = Length(@amount_of_deliveries)
+ IF @sl > 3 THEN
+
                 SET @l = Substring(@amount_of_deliveries,0,Subtract(@sl,3))
                 SET @r = Substring(@amount_of_deliveries,Subtract(@sl,3),3)
                 SET @amount_of_deliveries = Concat(@l,',',@r)
