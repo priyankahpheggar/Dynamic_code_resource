f30b70b6c76f35fa0201ce649e94e33260de8b22
priyankahpheggar <priyankahp.heggar@gmail.com>
2021-11-12 19:48:36 +0530

    fgf
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -1,9 +1,11 @@
 %%[
-    SET @amount_of_deliveries = '2004767889'
+    SET @amount_of_deliveries = RequestParameter('X')
   SET @sl = Length(@amount_of_deliveries)
+  IF @s1 > 3 THEN
                 SET @l = Substring(@amount_of_deliveries,0,Subtract(@sl,3))
                 SET @r = Substring(@amount_of_deliveries,Subtract(@sl,3),3)
-                 SET @amount_of_deliveries = Concat(@l,',',@r)
+                SET @amount_of_deliveries = Concat(@l,',',@r)
+  ENDIF
 ]%%
 %%=v(@amount_of_deliveries)=%%
 
