9186ca86f11e20f5814efa95d80dedd5c8ad4747
priyankahpheggar <priyankahp.heggar@gmail.com>
2021-11-12 20:04:07 +0530

    fgf
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -6,7 +6,19 @@
                 SET @l = Substring(@amount_of_deliveries,0,Subtract(@sl,3))
                 SET @r = Substring(@amount_of_deliveries,Subtract(@sl,3),3)
                 SET @amount_of_deliveries = Concat(@l,',',@r)
-  ENDIF
+
+                IF @sl > 6 THEN
+                SET @p1 = Substring(@amount_of_deliveries,0,Subtract(@sl,3))
+                SET @s1 = Substring(@p1,0,Subtract(@sl,3))
+                SET @s2 = Substring(@p1,Subtract(@sl,3),3)
+
+
+                SET @s3 = Substring(@amount_of_deliveries,Subtract(@sl,3),3)
+                SET @amount_of_deliveries = Concat(@s1,',',@s2,',',@s3)
+                ENDIF
+ENDIF
+  
 ]%%
 %%=v(@amount_of_deliveries)=%%
+
 
