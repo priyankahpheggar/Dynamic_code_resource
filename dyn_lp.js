2918b2cd83b5e1f1dcd927dfcc40f7bf586dd81f
priyankahpheggar <priyankahp.heggar@gmail.com>
2021-11-23 12:51:38 +0530

    f
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -2,8 +2,8 @@
 /* SET @cars = Lookup("DE_name", "cars", "Email", _subscriberkey) */
 SET @cars = "Alto   Tiago   Swift   Duster"
 SET @query = "Swift"
-IF (IndexOf(@cars,@query)) THEN
-Output(concat("Already exist"))
+IF (IndexOf(@cars,@query)) > 0 THEN
+Output(concat("Already exist",(IndexOf(@cars,@query))))
 ELSE
 Output(concat("Not exist"))
 ENDIF
