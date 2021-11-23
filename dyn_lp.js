5ad897e469de71827b8b35f17c503b866d489b1e
priyankahpheggar <priyankahp.heggar@gmail.com>
2021-11-23 10:41:20 +0530

    fg
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -1,24 +1,5 @@
 %%[
-    SET @amount = RequestParameter('x')
-    SET @L1 = Length(@amount)
-    
-    IF @L1 > 6 THEN
-        SET @l = Substring(@amount,1,Subtract(@L1,3))   //12345
-        SET @L2 = Length(@l)
-        SET @lp1 = Substring(@l,1,Subtract(@L2,3))   //12 
-        SET @rp1 = Substring(@l,Subtract(@L2,2),3)  //345
-
-        SET @r = Substring(@amount,Subtract(@L1,2),3)   //678
-        SET @FS = Concat(@lp1,',',@rp1,',',@r)
-    
-    ELSEIF @L1 > 3 THEN
-        SET @l = Substring(@amount,1,Subtract(@L1,3))   //12345 
-        SET @r = Substring(@amount,Subtract(@L1,2),3)   //678
-        SET @FS = Concat(@l,',',@r)
-
-    ENDIF
-
-
+Set @date = Now()
+Set @date = SystemDateToLocalDate(@date)
 ]%%
-<br>
-Output = %%=v(@FS)=%%
+%%=v(@date)=%%
