b3b164d07aa248d7f8ded42afddca68d24c1656e
priyankahpheggar <priyankahp.heggar@gmail.com>
2021-11-24 16:20:44 +0530

    g
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -7,6 +7,7 @@
     <title>材料注文_完了画面</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <style>
     * {
     padding: 0;
