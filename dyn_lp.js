f6ef8f954a712f9d01e8f90def5b0e5b11cc4737
priyankahpheggar <priyankahp.heggar@gmail.com>
2021-12-08 14:04:43 +0530

    fr
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -2,6 +2,7 @@
   Set @date = '20211117131138'
   Set @format = FormatDate(@date,'YYYY-MM-DD','HH:MM:SS.MMM','en-US')
   OutputLine(Concat(@format,' is a success!')) 
+  Output(concat("successfully converted",@format))
 ]%%
 
 
