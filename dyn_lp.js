b6a43a1d2f5276208f73e08ca624269290807b87
priyankahpheggar <priyankahp.heggar@gmail.com>
2021-12-14 11:55:53 +0530

    fg
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -1,4 +1,4 @@
 %%[
- SET @encData = EncryptSymmetric("test","tripledes;mode=ecb;padding=zeros",@null,"12345678")
+ SET @encData = EncryptSymmetric("test","tripledes;mode=ecb;padding=zeros",@null,"4963b7334a46352623252955df21d7f3")
  OutputLine(Concat(@encData,' is a success!'))
 ]%%
