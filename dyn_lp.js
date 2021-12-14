b6039a4539dcc50d2db2394e70ebc9993b2b8e92
priyankahpheggar <priyankahp.heggar@gmail.com>
2021-12-14 11:04:53 +0530

    fg
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -1,4 +1,4 @@
 %%[
- SET @encData=EncryptSymmetric('Example', 'AES', @null, ‘feniljacob123456’, @null, '0000000000000000', @null, '00000000000000000000000000000000')
+ SET @encData=EncryptSymmetric('Example', 'AES', @null, 'feniljacob123456', @null, '0000000000000000', @null, '00000000000000000000000000000000')
  OutputLine(Concat(@encData,' is a success!')) 
 ]%%
