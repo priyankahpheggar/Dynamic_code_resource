4ad31c643d175892348be6a6cfd35bc2bcd55f87
priyankahpheggar <priyankahp.heggar@gmail.com>
2021-12-24 18:37:45 +0530

    fg
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -1,13 +1,8 @@
 %%[
 SET @dcf = '198970'
-SET @decrypt_dcf = Base64Decode(@dcf)
-SET @mail_seq = RequestParameter('mail_seq')
-SET @site = "AMV"
-
 IF not empty (@dcf) THEN
 OutputLine(Concat(@dcf,'DCF member')) 
 ELSE
 OutputLine(Concat(@dcf,'NON-DCF member')) 
-ENDIF
-  
+ENDIF 
 ]%% 
