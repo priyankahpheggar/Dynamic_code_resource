9c6b81afb686c1c988186091c73f51c8934d7336
priyankahpheggar <priyankahp.heggar@gmail.com>
2022-01-03 20:02:39 +0530

    fdf
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -1,6 +1,8 @@
 %%[
 SET @fcm = Concat("01","/",Datepart(Now(),'M'),"/",Datepart(Now(),'Y'))
 SET @ldp = DateAdd(@fcm, '-1','D')
+SET @day = DatePart(@ldp, "Day")
 ]%%
 %%=v(@fcm)=%%<br>
 %%=v(@ldp)=%%<br>
+%%=v(@day)=%%<br>
