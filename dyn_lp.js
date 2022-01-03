4df912e19e0310a22574fd4e789a46c9b0dd717a
priyankahpheggar <priyankahp.heggar@gmail.com>
2022-01-03 20:10:15 +0530

    dfgf
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -1,7 +1,7 @@
 %%[
-SET @fcm = Concat("01","/",Datepart('01-FEB-2022','M'),"/",Datepart('01-FEB-2022','Y'))
+SET @fcm = Concat("01","/",Datepart('02-01-2022','M'),"/",Datepart('02-01-2022','Y'))
 SET @ldp = DateAdd(@fcm, '-1','D')
-SET @cm = Concat(DatePart(@ldp, "Day"),"/",Datepart('01-FEB-2022','M'),"/",Datepart('01-FEB-2022','Y'))
+SET @cm = Concat(DatePart(@ldp, "Day"),"/",Datepart('02-01-2022','M'),"/",Datepart('02-01-2022','Y'))
 ]%%
 %%=v(@fcm)=%%<br>
 %%=v(@ldp)=%%<br>
