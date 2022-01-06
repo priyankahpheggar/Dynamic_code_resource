d57cdf1f71c8c2bd57843134ede4c89548ed7a9f
priyankahpheggar <priyankahp.heggar@gmail.com>
2022-01-06 10:16:01 +0530

    fgfg
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -1,4 +1,5 @@
 %%[
-    SET @today = DateAdd(Now(), '-4','hh')
-    OutputLine(v(@today))
+    SET @today = DateAdd(Now(), '-4','h')
+
+    OutputLine(FormatDate(@today,'YYYY-MM-DD','HH:MM:SS.MMM','en-US'))
 ]%%
