9fffee733c469708f4746ae292f9db0c6df865cc
priyankahpheggar <priyankahp.heggar@gmail.com>
2022-01-07 16:39:11 +0530

    fgf
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -1,4 +1,24 @@
 %%[
-OutputLine(BuildRowsetFromString('Rose | Jasmine | sunflower', '|'))
+SET @car = "Alto,waganor,duster"
+SET @row = BuildRowsetFromString(@car, ',')
+SET  @RowCount = RowCount(@row)
+IF @RowCount > 1 THEN
+For @i = 1 to @RowCount do
+    set @row = row(@row, @i) /* get row based on counter */
+    set @car = field(@row,1)
+
+    ]%%
+
+    <br>Row %%=v(@i)=%%, car: %%=v(@car)=%%
+
+    %%[
+
+  next @i ]%%
+
+%%[ else ]%%
+
+No rows found
+
+%%[ endif ]%%
 ]%%
 
