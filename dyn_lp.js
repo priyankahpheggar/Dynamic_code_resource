24e38621f0da9abbb020aad03a8c10f0613166dc
priyankahpheggar <priyankahp.heggar@gmail.com>
2022-05-24 12:15:25 +0530

    fgfg
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -1,8 +1,12 @@
 %%[
-    var @date, @result
-    set @date = Now()
-    set @result = DateParse(@date,1)
-    
-    
-    ]%%
-    Output: %%=v(@result)=%%
+
+    set @dateString = "May 25 2022 12:00AM"
+    set @monthNumber = FormatDate(@dateString,"MM")
+    set @dayNumber = FormatDate(@dateString,"dd")
+    set @yearFull = FormatDate(@dateString,"YYYY")
+    set @hour = FormatDate(@timeString,"","hh")
+    set @minute = FormatDate(@timeString,"","mm")
+    set @seconds = FormatDate(@timeString,"","ss")
+    set @AMPM = FormatDate(@timeString,"","tt")
+    output(concat(@monthNumber,'/',@dayNumber,'/',@yearFull,' ',@hour,':',@minute,':',@seconds,' ',@AMPM))
+     ]%%
