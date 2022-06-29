fc496f0ea1858d480c44fbfe566edffa7ee14ac5
priyankahpheggar <priyankahp.heggar@gmail.com>
2022-06-29 14:24:32 +0530

    fgfg
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -1,4 +1,4 @@
-%%[
-SET @date = 'Thursday, June 30, 2022 9:30 AM'
-%%=v(FormatDate(@date, "DD/MMM/YYYY", "de"))=%%
-     ]%%
+
+%%=FormatDate('Thursday, June 30, 2022 9:30 AM', "DD/MMM/YYYY", "de")=%%
+
+
