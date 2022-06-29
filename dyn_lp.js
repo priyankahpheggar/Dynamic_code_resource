0fda2c2d2694abb4ccb6e1f7cf462eed1e9241fc
priyankahpheggar <priyankahp.heggar@gmail.com>
2022-06-29 14:21:55 +0530

    gf
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -1,12 +1,4 @@
 %%[
-
-    set @dateString = "May 25 2022 12:00AM"
-    set @monthNumber = FormatDate(@dateString,"MM")
-    set @dayNumber = FormatDate(@dateString,"dd")
-    set @yearFull = FormatDate(@dateString,"YYYY")
-    set @hour = FormatDate(@timeString,"","hh")
-    set @minute = FormatDate(@timeString,"","mm")
-    set @seconds = FormatDate(@timeString,"","ss")
-    set @AMPM = FormatDate(@timeString,"","tt")
-    output(concat(@monthNumber,'/',@dayNumber,'/',@yearFull,' ',@hour,':',@minute,':',@seconds,' ',@AMPM))
+SET @date = 'Thursday, June 30, 2022 9:30 AM'
+%%=FormatDate(@date, "DD/MMM/YYYY", "de")=%%
      ]%%
