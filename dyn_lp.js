0ba9ce48ab6a103f10415467d6342e083d84bbdb
priyankahpheggar <priyankahp.heggar@gmail.com>
2023-01-05 14:40:14 +0530

    fgfg
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -1,6 +1,6 @@
 %%[
 
-    SET @online_live_date = '2023-01-05 05:54 PM'
+    SET @online_live_date = '2023-01-05 06:12 PM'
     SET @Current_date = DateAdd(Now(), 15, "H")
     set @Current_date = Format(@Current_date, "yyyy-MM-dd HH:mm", "Date")
     SET @diff = DateDiff(@online_live_date,@Current_date, 'MI')
