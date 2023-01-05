15b48a3f04ea0a9104a021e50499c974be04f4d5
priyankahpheggar <priyankahp.heggar@gmail.com>
2023-01-05 14:39:17 +0530

    rtrt
    Signed-off-by: priyankahpheggar <priyankahp.heggar@gmail.com>

--- dyn_lp.js
+++ dyn_lp.js
@@ -1,7 +1,13 @@
-
 %%[
-    SET @nowGMT = DateAdd(Now(), 9, "H")
-    SET @formattedUTCDate = Format(@nowGMT, "yyyy-MM-ddTHH:mm:ssZ", "Date")
-]%%
 
-%%=v(@formattedUTCDate)=%%
+    SET @online_live_date = '2023-01-05 05:54 PM'
+    SET @Current_date = DateAdd(Now(), 15, "H")
+    set @Current_date = Format(@Current_date, "yyyy-MM-dd HH:mm", "Date")
+    SET @diff = DateDiff(@online_live_date,@Current_date, 'MI')
+    SET @Islive = IIF(@diff >= 1,'True','False')
+    
+    ]%%
+    online_live_date = %%=v(@online_live_date)=%% <br/>
+    Current_date = %%=v(@Current_date)=%% <br/>
+    diff = %%=v(@diff)=%% <br/>
+    Islive = %%=v(@Islive)=%% <br/>
